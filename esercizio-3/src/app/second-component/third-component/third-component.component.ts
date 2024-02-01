a24920f512ce60abf59d0d5a2530de356871bdd7
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2024-02-01 12:57:34 +0000

    PUNTO 6.1

--- esercizio-3/src/app/second-component/third-component/third-component.component.ts
+++ esercizio-3/src/app/second-component/third-component/third-component.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-third-component',
   standalone: true,
-  imports: [],
+  imports: [MatProgressBarModule, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './third-component.component.html',
   styleUrl: './third-component.component.css'
 })
-export class ThirdComponentComponent {
 
+export class ThirdComponentComponent {
+  
 }
